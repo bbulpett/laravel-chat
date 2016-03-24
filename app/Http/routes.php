72c6c7501b61766c734c09f3ce2d6e3bd2ab0124
Barnabas Bulpett <bbulpett@charter.net>
2016-03-24 08:18:35 -0400

    Pusher configs and routing for test channel

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,15 @@ Route::group(['middleware' => ['web']], function () {
     });
 
 });
+
+use Illuminate\Support\Facades\App;
+
+Route::get('/bridge', function() {
+    $pusher = App::make('pusher');
+
+    $pusher->trigger( 'test-channel',
+                      'test-event', 
+                      array('text' => 'Preparing the Pusher Laracon.eu workshop!'));
+
+    return view('welcome');
+});
